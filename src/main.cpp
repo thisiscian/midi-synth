6b1af8ee21adaa7a17bd11df050658aa08793df9
Cian Booth <this.is.cian@gmail.com>
2014-12-30 15:55:25 +0000

    added sdl support

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <cstdio>
 #include <iostream>
 #include <ncurses.h>
+#include <SDL2/SDL.h>
+
 #define _USE_MATH_DEFINES
 using namespace std;	
 class SoundInfo {
@@ -40,18 +42,22 @@ void generate_tone(snd_pcm_t *handle, int volume, int note, int pitch, float len
 	number_of_frames=(int) (length*(*(sound_info.sample_rate))/sound_info.channels);
 	data=(unsigned char *)malloc(number_of_frames*2*sound_info.channels);
 	for(int i=0; i<number_of_frames; ++i) {
-		short s=volume*(cos(M_PI/2+2*M_PI*i*frequency*length/(number_of_frames)));
+		short s=volume*(cos(M_PI/2+2*M_PI*i*frequency*length/(number_of_frames))+cos(M_PI/2+3*M_PI*i*frequency*length/(number_of_frames)));
 		for(int j=0; j<sound_info.channels; ++j) {
 			data[2*(i*sound_info.channels+j)+0]=s%256;
 			data[2*(i*sound_info.channels+j)+1]=s>>8;
 		}
 	}
 	int err;
+	printf("pcm state: %d\n", snd_pcm_(handle));
+	if(snd_pcm_state(handle)==SND_PCM_STATE_RUNNING) {
+	}
 	err=snd_pcm_writei(handle, data, number_of_frames);
 	if(err<0) {
-		err = snd_pcm_recover(handle, err, 0);
-		if(err=snd_pcm_prepare(handle)<0){error_output("cannot prepare audio interface for use",err);}
-		snd_pcm_drop(handle);
+		err=snd_pcm_recover(handle, err, 1);
+		snd_pcm_writei(handle, data, 1);
+		err=snd_pcm_writei(handle, data, number_of_frames);
+//		if(err=snd_pcm_prepare(handle)<0){error_output("cannot prepare audio interface for use",err);}
 	}
 	if(err<0){error_output("write to audio interface failed",err);}
 	return;
@@ -65,6 +71,9 @@ snd_pcm_t* initialise(const char* device="default" ) {
 	unsigned int rate_near=44100;	
 	snd_pcm_t *playback_handle;
 	snd_pcm_hw_params_t *hw_params;
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window * window = SDL_CreateWindow("SDL2 Keyboard/Mouse events", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+        
 	if (err=snd_pcm_open(&playback_handle, device, SND_PCM_STREAM_PLAYBACK, 0)<0) {
 		char *message;	
 		sprintf(message, "cannot open audio device %s", device);
@@ -77,9 +86,11 @@ snd_pcm_t* initialise(const char* device="default" ) {
 	if(err=snd_pcm_hw_params_set_format(playback_handle, hw_params, SND_PCM_FORMAT_S16_LE)<0){error_output("cannot set sample format",err);}
 	if(err=snd_pcm_hw_params_set_rate_near(playback_handle, hw_params, si.sample_rate, 0)<0){error_output("cannot set sample rate",err);}
 	if(err=snd_pcm_hw_params_set_channels(playback_handle, hw_params, si.channels)<0){error_output("cannot set channel count",err);}
+	if(err=snd_pcm_hw_params_set_channels(playback_handle, hw_params, si.channels)<0){error_output("cannot set channel count",err);}
 	if(err=snd_pcm_hw_params(playback_handle, hw_params)<0){error_output("cannot set parameters",err);}
 	snd_pcm_hw_params_free(hw_params);
 	if(err=snd_pcm_prepare(playback_handle)<0){error_output("cannot prepare audio interface for use",err);}
+	
 	return playback_handle;	
 }
 
@@ -96,46 +107,36 @@ int main (int argc, char *argv[]) {
 	}
 	printf("done\n");
 
-	bool continue_playing=false;
-	int osc=0;
-	char note;
 	int pitch=4;
-	int note_num=-1;
-	initscr();
-	erase();
-	refresh();
-	cbreak();
-	noecho();
-	while(!continue_playing) {
-		note=getch();
-		refresh();
-		erase();
-		refresh();
-		switch(note) {
-			case 'a': note_num=0; printw("playing C\n"); break;
-			case 'w': note_num=1; printw("playing C#\n"); break;
-			case 's': note_num=2; printw("playing D\n"); break;
-			case 'e': note_num=3; printw("playing Eb\n"); break;
-			case 'd': note_num=4; printw("playing E\n"); break;
-			case 'f': note_num=5; printw("playing F\n"); break;
-			case 't': note_num=6; printw("playing F#\n"); break;
-			case 'g': note_num=7; printw("playing G\n"); break;
-			case 'y': note_num=8; printw("playing G#\n"); break;
-			case 'h': note_num=9; printw("playing A\n"); break;
-			case 'u': note_num=10; printw("playing Bb\n"); break;
-			case 'j': note_num=11; printw("playing B\n"); break;
-			case 'q':
-				snd_pcm_close(playback_handle);
-				refresh();
-				endwin();
-				exit(0);
-			default:
-				note_num=-1;
-				printw("nothing %c\n", note);
-				break;
+	SDL_Event event;
+	int volume=5000;
+	while(true) {
+		int note_num=-1;
+		SDL_PollEvent(&event);
+		if (event.type!=SDL_KEYDOWN) {
+			continue;
+		} else if(event.type==SDL_QUIT) {
+			exit(1);	
 		}
+		switch(event.key.keysym.sym) {
+			case SDLK_EQUALS: volume+=1000; printf("raising volume\n"); break;
+			case SDLK_MINUS: volume-=1000; printf("lowering volume\n"); break;
+			case SDLK_a: note_num=0; printf("playing C\n"); break;
+			case SDLK_w: note_num=1; printf("playing C#\n"); break;
+			case SDLK_s: note_num=2; printf("playing D\n"); break;
+			case SDLK_e: note_num=3; printf("playing Eb\n"); break;
+			case SDLK_d: note_num=4; printf("playing E\n"); break;
+			case SDLK_f: note_num=5; printf("playing F\n"); break;
+			case SDLK_t: note_num=6; printf("playing F#\n"); break;
+			case SDLK_g: note_num=7; printf("playing G\n"); break;
+			case SDLK_y: note_num=8; printf("playing G#\n"); break;
+			case SDLK_h: note_num=9; printf("playing A\n"); break;
+			case SDLK_u: note_num=10; printf("playing Bb\n"); break;
+			case SDLK_j: note_num=11; printf("playing B\n"); break;
+			case SDLK_q: snd_pcm_close(playback_handle); SDL_Quit(); exit(0); break;
+		}	
 		if(note_num != -1) { 
-			generate_tone(playback_handle, 5000, note_num, pitch, 0.1);
+			generate_tone(playback_handle, volume, note_num, pitch, 0.1);
 		}
 	}
 	sleep(1);
